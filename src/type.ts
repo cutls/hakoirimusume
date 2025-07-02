ce2d4a0d33fa38fb8b8d2f87ff72fb5dd3e7bb5c
cutls <p@cutls.dev>
2025-07-03 01:23:08 +0900

    too easy

--- src/type.ts
+++ src/type.ts
@@ -2,39 +2,23 @@ export const keys = ['father', 'daughter', 'mother', 'tedai', 'daibantou', 'aniy
 export type Key = (typeof keys)[number]
 export type StateType = Record<Key, { x: number; y: number; width: number; height: number, name: string, kana?: string }>
 
-// export const step1: StateType = {
-//     father: { x: 2, y: 0, width: 1, height: 2, name: '父' },
-//     daughter: { x: 3, y: 0, width: 2, height: 2, name: '娘' },
-//     mother: { x: 5, y: 0, width: 1, height: 2, name: '母' },
-//     tedai: { x: 0, y: 2, width: 1, height: 1, name: '手代', kana: 'てだい' },
-//     daibantou: { x: 1, y: 2, width: 4, height: 1, name: '大番頭' },
-//     aniyome: { x: 5, y: 2, width: 1, height: 1, name: '兄嫁' },
-//     detchi1: { x: 0, y: 3, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
-//     detchi2: { x: 5, y: 3, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
-//     jochu: { x: 1, y: 3, width: 2, height: 1, name: '女中' },
-//     bandou: { x: 3, y: 3, width: 2, height: 1, name: '番頭' },
-//     detchi3: { x: 5, y: 4, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
-//     banken: { x: 0, y: 4, width: 1, height: 1, name: '番犬' },
-//     sofu: { x: 1, y: 4, width: 2, height: 1, name: '祖父' },
-//     sobo: { x: 3, y: 4, width: 2, height: 1, name: '祖母' }
-// }
-
 export const step1: StateType = {
     father: { x: 2, y: 0, width: 1, height: 2, name: '父' },
-    daughter: { x: 2, y: 3, width: 2, height: 2, name: '娘' },
+    daughter: { x: 3, y: 0, width: 2, height: 2, name: '娘' },
     mother: { x: 5, y: 0, width: 1, height: 2, name: '母' },
     tedai: { x: 0, y: 2, width: 1, height: 1, name: '手代', kana: 'てだい' },
     daibantou: { x: 1, y: 2, width: 4, height: 1, name: '大番頭' },
     aniyome: { x: 5, y: 2, width: 1, height: 1, name: '兄嫁' },
     detchi1: { x: 0, y: 3, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
     detchi2: { x: 5, y: 3, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
-    jochu: { x: 0, y: 0, width: 2, height: 1, name: '女中' },
-    bandou: { x: 3, y: 0, width: 2, height: 1, name: '番頭' },
+    jochu: { x: 1, y: 3, width: 2, height: 1, name: '女中' },
+    bandou: { x: 3, y: 3, width: 2, height: 1, name: '番頭' },
     detchi3: { x: 5, y: 4, width: 1, height: 1, name: '丁稚', kana: 'でっち' },
     banken: { x: 0, y: 4, width: 1, height: 1, name: '番犬' },
-    sofu: { x: 0, y: 1, width: 2, height: 1, name: '祖父' },
-    sobo: { x: 3, y: 1, width: 2, height: 1, name: '祖母' }
+    sofu: { x: 1, y: 4, width: 2, height: 1, name: '祖父' },
+    sobo: { x: 3, y: 4, width: 2, height: 1, name: '祖母' }
 }
+
 export const step2: StateType = {
     father: { x: 0, y: 2, width: 1, height: 2, name: '父' },
     daughter: { x: 1, y: 0, width: 2, height: 2, name: '娘' },
